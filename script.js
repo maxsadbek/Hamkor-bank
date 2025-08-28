ec189aea9577e6558ca127b9e7676d9bafff1d0a
maxsad_bek <masadanb@gmail.com>
2025-08-28 12:12:19 +0500

    add

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let searchBtn = document.querySelector(".search-box")
 let searchInput = document.querySelector("#search")
 let pBtn = document.querySelector(".p")
 let dropdownBtn = document.querySelector("#dropdown")
+let menuBtn = document.querySelector("#menuBtn")
+let menuBox = document.querySelector(".menu_box")
+
+menuBtn.addEventListener("click", function(){
+    menuBox.classList.toggle("active")
+})
 
 pBtn.addEventListener("click", function(){
     dropdownBtn.classList.toggle("active")
