f5465de867daf25c83833be4c41dda82a2a90920
maxsad_bek <masadanb@gmail.com>
2025-08-28 11:00:30 +0500

    add

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let searchBtn = document.querySelector(".search-box")
 let searchInput = document.querySelector("#search")
+let pBtn = document.querySelector(".p")
+let dropdownBtn = document.querySelector("#dropdown")
+
+pBtn.addEventListener("click", function(){
+    dropdownBtn.classList.toggle("active")
+})
+
 
 searchInput.addEventListener("click", function(){
     searchBtn.classList.toggle("active")
